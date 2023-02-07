9445a19a7adf54db59691e29bdfc48012ef77dfd
alcl2000 <amy.555.lewis@gmail.com>
2023-02-07 15:09:37 +0000

    fix: app added to allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost", "i-think.herokuapp.com"]
 
 
 # Application definition
